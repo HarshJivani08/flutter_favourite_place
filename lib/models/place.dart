5db5fd3120e5dbacaea5059dd5c8cda0a0d6ccac
HarshJivani08 <harshjivani140901@gmail.com>
2024-06-08 10:42:55 +0530

    set local_database

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -17,7 +17,9 @@ class Place {
     required this.title,
     required this.image,
     required this.placeLocation,
-  }) : id = uuid.v4();
+    String? id,
+  }) : id = id ?? uuid.v4();
+
   final String id;
   final String title;
   final File image;
